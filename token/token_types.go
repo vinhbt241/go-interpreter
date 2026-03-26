d954d8bd1f65dc08ea7bb6222b1185d28f0d1a38
Vinh <vinhbuith32412000@gmail.com>
2026-03-26 20:25:10 +0700

    add more one-character token type

--- token/token_types.go
+++ token/token_types.go
@@ -9,8 +9,14 @@ const (
 	INT   = "INT"   //123456
 
 	// operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	GT       = "<"
+	LT       = ">"
 
 	// delimiters
 	COMMA     = ","
