f3c76c3f07ab412c700ffd53d79194d19d6f55eb
Vinh <vinhbuith32412000@gmail.com>
2026-03-30 23:22:59 +0700

    bind identifiers

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -180,6 +180,10 @@ func TestErrorHandling(t *testing.T) {
 			`,
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
+		{
+			"foobar",
+			"identifier not found: foobar",
+		},
 	}
 
 	for _, tt := range tests {
@@ -193,14 +197,33 @@ func TestErrorHandling(t *testing.T) {
 	}
 }
 
+func TestLetStatement(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let a = 5; a;", 5},
+		{"let a = 5 * 5; a;", 25},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+	}
+
+	for _, tt := range tests {
+		evaluated := prepareObject(tt.input)
+
+		assertIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
 //helpers
 
 func prepareObject(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
 	program := p.ParseProgram()
+	env := object.NewEnvironment()
 
-	return Eval(program)
+	return Eval(program, env)
 }
 
 func assertObjectType[T object.Object](t testing.TB, got object.Object) T {
