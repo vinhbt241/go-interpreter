539a00326ba50565ea9f8d5dc144b6f8eaafd582
Vinh <vinhbuith32412000@gmail.com>
2026-03-31 00:05:10 +0700

    evaluate call expression

--- object/object.go
+++ object/object.go
@@ -58,15 +58,19 @@ func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
 
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
-	return &Environment{store: s}
+	return &Environment{store: s, outer: nil}
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
@@ -98,4 +102,10 @@ func (f *Function) Inspect() string {
 	out.WriteString("\n")
 
 	return out.String()
+}
+
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
 }
