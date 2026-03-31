55c4df2e6453c275038a214e6f2706fb0303fd8e
Vinh <vinhbuith32412000@gmail.com>
2026-03-31 14:55:09 +0700

    evaluate array index expression

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -340,6 +340,64 @@ func TestArrayLiterals(t *testing.T) {
 	assertIntegerObject(t, result.Elements[2], 6)
 }
 
+func TestArrayIndexExpressions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected any
+	}{
+		{
+			"[1, 2, 3][0]",
+			1,
+		},
+		{
+			"[1, 2, 3][1]",
+			2,
+		},
+		{
+			"[1, 2, 3][2]",
+			3,
+		},
+		{
+			"let i = 0; [1][i];",
+			1,
+		},
+		{
+			"[1, 2, 3][1 + 1];",
+			3,
+		},
+		{
+			"let myArray = [1, 2, 3]; myArray[2];",
+			3,
+		},
+		{
+			"let myArray = [1, 2, 3]; myArray[0] + myArray[1] + myArray[2];",
+			6,
+		},
+		{
+			"let myArray = [1, 2, 3]; let i = myArray[0]; myArray[i]",
+			2,
+		},
+		{
+			"[1, 2, 3][3]",
+			nil,
+		},
+		{
+			"[1, 2, 3][-1]",
+			nil,
+		},
+	}
+
+	for _, tt := range tests {
+		evaluated := prepareObject(tt.input)
+		integer, ok := tt.expected.(int)
+		if ok {
+			assertIntegerObject(t, evaluated, int64(integer))
+		} else {
+			assertNullObject(t, evaluated)
+		}
+	}
+}
+
 //helpers
 
 func prepareObject(input string) object.Object {
